32d0c895cad07f906b8e8fdee21bfa6c3fd25c47
Jonas Solsvik <jonas.solsvik@gmail.com>
2017-03-23 13:49:58 +0100

    Added top description to main.cpp

---
+++ source/main.cpp
@@ -0,0 +1,8 @@
+//
+// @file    main.cpp
+// @repo    gruppe32
+// @created 23.03.17 by Jonas
+// @brief   Main hub of the application. Glues together
+//           Core-module with the User-module
+//
+
