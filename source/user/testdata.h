845f5eae89f13bb868e48e83765dda4e63645f0f
Jonas Solsvik <jonas.solsvik@gmail.com>
2017-03-23 18:32:24 +0100

    Documented testdata-fields

--- source/user/testdata.h
+++ source/user/testdata.h
@@ -1,4 +1,12 @@
-
+//
+// @file    user/testdata.h
+// @repo    gruppe32
+// @created 23.03.17 by Jonas
+// @brief     a collection of test data that simulate data
+//            which controller recieves from the database.
+//            This could also serves as a standard for the dataformat,
+//            on different containers throughout the program.
+//
 
 
 #include <vector>
@@ -6,54 +14,53 @@
 
 //
 // @namespace test - testdata
-// @brief     a collection of test data that simulate data
-//            which controller recieves from the database.
 //
 namespace test {
   std::vector<std::string> 
   nation 
   {
-      "Norge", 
-      "NOR", 
-      "100",
-      "452000864",
-      "jonas.solsvik@gmail.com",
+      "NOR",                      // Code
+      "Norge",                    // Name
+      "150",                      // Number of athletes
+      "Jonas"                     // Name of contact
+      "452000864",                // Phone
+      "jonas.solsvik@gmail.com",  // Email
   };
 
   std::vector<std::string>
   athlete 
   {
-      "1002", 
-      "Jonas",
-      "452000864",
-      "jonas.solsvik@gmail.com", 
-      "100",
-      "Male"
+      "1002",                    // ID
+      "Jonas",                   // Name
+      "452000864",               // Phone
+      "jonas.solsvik@gmail.com", // Email
+      "NOR",                     // Nation Code 
+      "Male"                     // enum Gender
   };
 
   std::vector<std::string>
   sport 
   {
-      "Fotball", 
-      "Points",
-      "3", 
-      "Event1",
-      "Event2",
-      "Event3",
+      "Fotball",                // Sport name
+      "Points",                 // Score type
+      "3",                      // Number of events
+      "Event1",                 // Event 1
+      "Event2",                 // Event 2
+      "Event3",                 // Event 3
   };
 
   std::vector<std::string>
   event 
   {
-      "Finale - Golden Goal",
-      "19:00",
-      "29.03.18",
-      "2",
-      "Oddvar FK",
-      "Brå BK",
-      "2",
-      "1",
-      "0",
+      "Finale - Golden Goal",   // Name of event
+      "19:00",                  // Time of event
+      "29.03.18",               // Date of event
+      "2",                      // number of Athletes
+      "1001",                   // Athlete 1
+      "1002",                   // Athlete 2
+      "2",                      // number of Results
+      "1",                      // Result 1
+      "0",                      // Result 2
   };
 
   std::vector<std::vector<std::string>>
