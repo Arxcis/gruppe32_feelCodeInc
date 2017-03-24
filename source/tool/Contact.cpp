127e652b5b6f268365c1dfb39633e7e2f7fd94ae
Jonas Solsvik <jonas.solsvik@gmail.com>
2017-03-24 17:22:04 +0100

    ref #8

--- source/tool/Contact.cpp
+++ source/tool/Contact.cpp
@@ -1,6 +1,18 @@
+#pragma once
 //
 // @file    tool/Contact.cpp
 // @repo    gruppe32
 // @created 24.03.17 by Jonas
 // @brief   A struct that holds Contact data to Participants
 //
+
+#include <string>
+
+struct Contact 
+{
+  
+private:
+  std::string name;
+
+
+};
