c883030e0681f8a9e0dcbe7886cd71ddcb41e72d
Jonas Solsvik <jonas.solsvik@gmail.com>
2017-03-25 09:44:14 +0100

    ref #34 - Header-file spec

---
+++ source/tool/valid.h
@@ -0,0 +1,22 @@
+#pragma once
+//
+// @file    tool/valid.h
+// @repo    gruppe32
+// @created 25.03.17 by Jonas
+// @brief   Validates if a string is one of the basic data types
+//
+
+
+#include <string>
+
+namespace valid
+{
+  bool isInt(const std::string& value);
+  bool isDouble(const std::string& value);
+  bool isPhone(const std::string& value);
+  bool isEmail(const std::string& value);
+  bool isPhone(const std::string& value);
+  bool isTime(const std::string& value);
+  bool isDate(const std::string& value);
+  bool isMedals(const std::string& value);
+}
