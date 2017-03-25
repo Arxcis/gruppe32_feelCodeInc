92600555c66e6e54efdbdb792a207f40fbdf141e
Jonas Solsvik <jonas.solsvik@gmail.com>
2017-03-25 14:00:27 +0100

    ref #34 - Naive implementation of validation, should be replaced by some regular expressions later

--- source/tool/valid.cpp
+++ source/tool/valid.cpp
@@ -1,1 +1,54 @@
 #include "valid.h"
+
+bool valid::isInt(const std::string& value)
+{
+  try
+  {
+    int test = stoi(value);
+    return true;
+  }
+  catch (int e)
+  {
+    return false;
+  }
+}
+
+bool valid::isDouble(const std::string& value)
+{
+  try
+  {
+    float test = stof(value);
+    return true;
+  }
+  catch (int e)
+  {
+    return false;
+  }
+}
+
+bool valid::isEmail(const std::string& value)
+{
+  return (value.find("@") != std::string::npos);
+}
+
+bool valid::isPhone(const std::string& value)
+{
+  return (value.find("+") != std::string::npos);
+}
+
+bool valid::isTime(const std::string& value)
+{
+  return (value.find(":",2) != std::string::npos) && 
+         (value.find(":",4) != std::string::npos);
+}
+
+bool valid::isDate(const std::string& value)
+{
+  return (value.find(".",2) != std::string::npos) &&
+         (value.find(".",4) != std::string::npos);
+}
+
+bool valid::isMedals(const std::string& value)
+{
+  return (value.find("-") != std::string::npos);
+}
