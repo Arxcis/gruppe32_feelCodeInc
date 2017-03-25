e1cc0fa460ddc6d69abd3355c8aced036d281a9e
Jonas Solsvik <jonas.solsvik@gmail.com>
2017-03-25 04:17:23 +0100

    Emptied main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -2,122 +2,13 @@
 // @file    main.cpp
 // @repo    gruppe32
 // @created 23.03.17 by Jonas
-// @brief   Main hub of the application. Glues together
-//           Core-module with the User-module
+// @brief   main.cpp
 //
 
-#include <tuple>
-#include <vector>
-#include <string>
-#include <stdio.h>  
-#include <stdlib.h>
+#include <stdio.h>
 
-#include "data/testData.h"
-
-
-enum Entity 
-{
-  NATION,
-  ATHLETE, 
-  SPORT,
-  EVENT,
-};
-
-
-// @namespace API - Application Layer Interface
-// @brief     API that the user-level code can use to communicate with,
-//              application layer.
-// 
-namespace API 
-{ 
-  bool add    (std::vector<std::string>* container);
-  bool addAll (std::vector<std::vector<std::string>>* containers);
-  bool update (std::vector<std::string>* container);
-  bool remove (Entity entity, std::string id);
-  auto get    (Entity entity, std::string id) -> std::vector<std::string>*;
-  auto getAll (Entity entity)  -> std::vector<std::vector<std::string>>*;
-  void quit();
-};
-
-//
-// @namespace test - testdata
-//
 int main()
 { 
   printf("Hei main.cpp\n");
   return 0;
-}
-
-
-namespace API 
-{
-  //
-  // @function add()
-  //
-  bool add( std::vector<std::string>* container)
-  {  return 1;  }
-
-
-  bool addAll (std::vector<std::vector<std::string>>* containers) 
-  { return 1; }
-
-  //
-  // @function edit()
-  //
-  bool update (std::vector<std::string>* container)
-  {  return 1;  }
-
-  //
-  // @function remove()
-  //
-  bool remove(Entity entity, std::string id)
-  {  return 1;  }
-
-  //
-  // @function get()
-  //
-  auto get(Entity entity, std::string id) -> std::vector<std::string>*
-  {
-    switch(entity)
-    {
-      case NATION: 
-        return &test::nation;
-      case ATHLETE:
-        return &test::athlete;
-      case SPORT:
-        return &test::sport;
-      case EVENT:
-        return &test::event;
-      default:
-        return test::nulldata;
-    }
-  }
-
-  //
-  // @function getAll()
-  //
-  auto getAll(Entity entity)  -> std::vector<std::vector<std::string>>*
-  {
-    switch(entity)
-    {
-      case NATION: 
-        return &test::nations;
-      case ATHLETE:
-        return &test::athletes;
-      case SPORT:
-        return &test::sports;
-      case EVENT: 
-        return &test::events;
-      default: 
-        return test::nulldatas;
-    }
-  }
-
-  //
-  // @function quit()
-  //
-  void quit()
-  {
-    
-  }
 }
