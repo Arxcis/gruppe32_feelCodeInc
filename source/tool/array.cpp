45fa0cd8ac7ce6e007719f3e379753666c121196
Halvor Bakken Smedås <stektpotet@gmail.com>
2017-03-28 14:35:11 +0200

    added bracket operator to arrays

--- source/tool/array.cpp
+++ source/tool/array.cpp
@@ -5,3 +5,7 @@ size_t dat::array<T, arr_size>::Count()
 {
 	return count_;
 }
+
+template<typename T, size_t arr_size>
+inline T & dat::array<T, arr_size>::operator[](int x)
+{ return values[x]; }
--- source/tool/array.h
+++ source/tool/array.h
@@ -1,5 +1,11 @@
 #pragma once
 
+// @file    core/Participant.h
+// @repo    gruppe32
+// @created 24.03.17 by Halvor
+// @brief   A collection type with minor improvements,
+//			mainly being the inclusion of a length member.
+
 #include <stddef.h>
 
 namespace dat
@@ -9,10 +15,10 @@ namespace dat
 	{
 	private:
 		size_t count_;
-
+    T values[arr_size];
 	public:
-		T values[arr_size];
 		const size_t length = arr_size;
 		size_t Count();
+		inline T& operator[] (int x);
 	};
 }
