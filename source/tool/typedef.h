a705f62cb8c3fe63cc8121718a73f615051cf6a9
Jonas Solsvik <jonas.solsvik@gmail.com>
2017-03-29 11:24:13 +0200

    typedef.h More comprehensive commenting

--- source/tool/typedef.h
+++ source/tool/typedef.h
@@ -1,9 +1,10 @@
 #pragma once
 //
-// @file    tool/char3.h
+// @file    tool/typedef.h
 // @repo    gruppe32
 // @created 24.03.17 by Halvor
-// @brief   All typedefs
+// @brief   All typedefs. Contains many of the most important data-transmitting objects in the
+//            program - dat::Container, dat::Object, dat::Field
 //
 
 #include <string>
@@ -11,17 +12,37 @@
 #include <vector>
 
 namespace dat
-{
-    //a defined char-array with space for 3 elements
-    
+{   
+    //
+    // @typedef dat::GenericField and dat::GenericContainer - Used to build up more specialized
+    //               containers and fields.
+    // 
     template<typename T, typename U>
-    using GenericField = std::pair<T, U>;                 // a key/value-pair      
-    using Field = GenericField<std::string, std::string>; // pair of strings
+    using GenericField = std::pair<T, U>;      // a key/value-pair of key-type T and value-type U
 
     template<typename T>
-    using GenericContainer = std::vector<T>; //lists all fields
-    using Object = GenericContainer<Field>;  //lists all fields as strings
+    using GenericContainer = std::vector<T>;   // A variable length array(vector) of types.
 
-    using Container = GenericContainer<Object>; //holds a list of all objects
+    //
+    // @typedef(using) dat::Field is used throughout the code as a key-value pair of strings.
+    //           All fields in all objects are evntually passed around as a collection of dat::Fields.
+    //    
+    using Field = GenericField<std::string, std::string>;
+
+
+    //
+    // @typedef(using) dat::Object - Used as a generic way of transmitting data about objects throughout
+    //                         the code. For instance, Nation, PArticipant,
+    //                         To see the format of how these objects should be built up, see 
+    //                         data/testData.h and data/*.format -files.
+    //                         Does only hold basic data of std::string type. 
+    //    
+    using Object = GenericContainer<Field>; 
+
+    //
+    // @typedef(using) dat::Container - Used when we want to transmit a collection of object which are of 
+    //                            the same type. Does only hold basic data of std::string type.
+    //    
+    using Container = GenericContainer<Object>; 
 
 }
