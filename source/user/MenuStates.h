89d3d0d40a0dad8e21176d20f5bf45df7056ea4f
Jonas Solsvik <jonas.solsvik@gmail.com>
2017-03-29 02:26:09 +0200

    revived MenuStates.h

---
+++ source/user/MenuStates.h
@@ -0,0 +1,11 @@
+#pragma
+
+//
+// @file    user/MenuStates.cpp
+// @repo    gruppe32
+// @created 29.03.17 by Jonas
+// @brief   Revived this file from the dead(commit  06219ee)
+//          Turns out that it also is a usefull thing to tell apart different
+//          integer-representations of states in the new object-oriented
+//          state system after all.
+//
