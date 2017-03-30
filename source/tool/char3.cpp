195c7a671b91528029d6b0c6291ff7ee6a5d5fbd
Halvor <stektpotet@gmail.com>
2017-03-30 23:55:57 +0200

    implemented casting of char3

--- source/tool/char3.cpp
+++ source/tool/char3.cpp
@@ -6,7 +6,7 @@ namespace dat {
   char3::char3(const char* s)
   {
 
-#ifdef WIN_32
+#ifdef WIN32
       strcpy_s(str, s);
 #else
       strcpy(str, s);
@@ -14,7 +14,7 @@ namespace dat {
       
   }
   inline char & char3::operator[](int i)
-  {
-    return str[i];
-  }
+  { return str[i]; }
+  char3::operator char*() const
+  {return (char*)str; }
 }
