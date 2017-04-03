59e613021dccea90656a469ceddde3469c157b99
Jonas Solsvik <jonas.solsvik@gmail.com>
2017-04-04 00:57:47 +0200

    ref #45 Laget nasjoner.dtax og deltagere.dtax(templates), og config.h #42

---
+++ source/config.h
@@ -0,0 +1,18 @@
+#pragma once
+//
+// @file    config.h
+// @repo    gruppe32
+// @created 04.04.17 by Halvor
+// @brief   We will try to keep constants here which have a big impact on the 
+//          CONFIGURATION of the system.
+//
+
+#include <string>
+
+const std::string nationsFile      = "data/oblig/nasjoner.dta";
+const std::string participantsFile = "data/oblig/deltagere.dta";
+const std::string sportsFile       = "data/oblig/grener.dta";
+
+const std::string medalsFile       = "data/oblig/poeng.dta";
+const std::string pointsFile       = "data/oblig/medaljer.dta";
+const std::string diciplinePath    = "data/oblig/";
