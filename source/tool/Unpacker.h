91bc7d46d892ff89a16d54a50140cdfb66bab2d8
Jonas Solsvik <jonas.solsvik@gmail.com>
2017-04-05 01:28:41 +0200

    Unpacker.h - packing::unpackTimeResult and packing::unpackPointResult

--- source/tool/Unpacker.h
+++ source/tool/Unpacker.h
@@ -8,8 +8,10 @@
 
 namespace dat { namespace packing
 {
-  Contact unpackContact (const Field & name_, const Field & phone_, const Field & mailAddress_);
-  Time    unpackTime    (const Field & time_);
-  Date    unpackDate    (const Field & date_);
-  Medals  unpackMedals  (const Field & medals);
+  Contact    unpackContact     (const Field&  name_, const Field & phone_, const Field & mailAddress_);
+  Time       unpackTime        (const Field&  time_);
+  Date       unpackDate        (const Field&  date_);
+  Medals     unpackMedals      (const Field&  medals_);
+  Result     unpackPointResult (const Object& pointRes_);
+  TimeResult unpackTimeResult  (const Object& timeRes_);
 }}
