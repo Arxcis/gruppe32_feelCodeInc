b62b2a60c3b2b1ac1169c6f0cc2100630d16c8be
Jonas Solsvik <jonas.solsvik@gmail.com>
2017-04-05 13:57:37 +0200

    remail regex

--- source/tool/valid.h
+++ source/tool/valid.h
@@ -18,13 +18,14 @@ namespace valid
   //   All these regular expressions har hand-made using the
   //    really helpfull tool rubuluar.com
   //
-  static std::regex regChar3  ("[A-Z]{3}");
-  static std::regex regInt    ("[0-9]+");
-  static std::regex regDouble ("[+-]?[0-9]{0,15}[\\.]?[0-9]{1,15}"); // @robustness - could overflow floating point
-  static std::regex regPhone  ("[+][0-9]{1,3}[ ](([0-9]{8})|(([0-9]{2}[ ]){3}[0-9]{2})|([0-9]{3}[ ][0-9]{2}[ ][0-9]{3}))");
-  static std::regex regTime   ("[0-9]{2}[:][0-9]{2}([:][0-9]{2}([:][0-9]{3})?)?"); // @friendlyness, demands 00:00:00:999 for milliseconds
-  static std::regex regDate   ("(([0][1-9])|([1-2][0-9])|([3][0-1]))[\\.](([0][1-9])|([1][0-2]))[\\.](([0-9]{2})|(([1][9][0-9]{2})|([2][0]([0-9]{2}))))"); //  @robustness - Does not account for varying month-length and year-length
-  static std::regex regMedals ("(([0][1-9]?)|[1-9]{1,2})([-](([0][1-9]?)|[1-9]{1,2})){2}");
+  static std::regex regChar3  ("^[A-Z]{3}$");
+  static std::regex regInt    ("^[0-9]+$");
+  static std::regex regDouble ("^[+-]?[0-9]{0,15}[\\.]?[0-9]{1,15}$"); // @robustness - could overflow floating point
+  static std::regex regPhone  ("^[+][0-9]{1,3}[ ](([0-9]{8})|(([0-9]{2}[ ]){3}[0-9]{2})|([0-9]{3}[ ][0-9]{2}[ ][0-9]{3}))$");
+  static std::regex regTime   ("^[0-9]{2}[:][0-9]{2}([:][0-9]{2}([:][0-9]{3})?)?$"); // @friendlyness, demands 00:00:00:999 for milliseconds
+  static std::regex regDate   ("^(([0][1-9])|([1-2][0-9])|([3][0-1]))[\\.](([0][1-9])|([1][0-2]))[\\.](([0-9]{2})|(([1][9][0-9]{2})|([2][0]([0-9]{2}))))$"); //  @robustness - Does not account for varying month-length and year-length
+  static std::regex regMedals ("^(([0][1-9]?)|[1-9]{1,2})([-](([0][1-9]?)|[1-9]{1,2})){2}$");
+  static std::regex regEmail  ("^[A-Za-z0-9\\.-_]{3,128}*[@][A-Za-z0-9\\.]{3,128}[.][a-z]{2,6}$");  // @robustness - pseudo email address.
 
 
   bool isChar3(const std::string& value);
