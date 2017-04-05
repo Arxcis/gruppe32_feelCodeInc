c6b717f8605af2f45336c6301f43ceb7737fec3c
Jonas Solsvik <jonas.solsvik@gmail.com>
2017-04-05 16:34:21 +0200

    Commented on how some of the REGEX-es work

--- source/tool/valid.h
+++ source/tool/valid.h
@@ -28,12 +28,52 @@ namespace valid
   static std::regex regEmail  ("([A-Za-z0-9\\.-_]{3,128})*[@][A-Za-z0-9\\.]{3,128}[.][a-z]{2,6}");  // @robustness - pseudo email address.
 
 
+  //
+  // @brief
+  //  @regex validates to true if:
+  //      1. 3 letter word
+  //      2. All uppercase
+  //      3. A-Z
+  //
   bool isChar3(const std::string& value);
+
+  //
+  // @brief
+  //  @regex validates to true if:
+  //      - A string consisting only of numeric-characters 0-9
+  //
   bool isInt(const std::string& value);
+
+  //
+  // @brief
+  //  @regex validates to true if:
+  //     1. 0-15 numberic characters,
+  //     2. followed by a period, 
+  //     3. followd by 1-15 numeric characters.
+  //
   bool isDouble(const std::string& value);
+
+  //
+  // @brief
+  //  @regex validates to true if:
+  //     1. Mandatory + at beginning
+  //     2. 1,2 or 3 numbers
+  //     3. Mandatory space
+  //     4. 8 numerics in one of these patterns xxx xx xxx, or xx xx xx xx or xxxxxxxx
+  //
   bool isPhone(const std::string& value);
+
+  //
+  // @brief
+  //  @regex validates to true if:
+  //     1. Minimum of 3 aplha-numeric character or _, -, . jonas.solsvik
+  //     2. Mandatory @
+  //     3. Minimum of 3 aplha-numeric character or .   ex. stud.ntnu
+  //     4. Mandatory . 
+  //     5. Betwenn 2-6 lowercase alpha-letters.    ex. com or no
+  //
   bool isEmail(const std::string& value);
-  bool isPhone(const std::string& value);
+  
   bool isTime(const std::string& value);
   bool isDate(const std::string& value);
   bool isMedals(const std::string& value);
