31a02fc0ace577289af33c4f02a5b095e70f80ac
Jonas Solsvik <jonas.solsvik@gmail.com>
2017-04-06 06:22:56 +0200

    Improved date REGEX + converted strings to regex-literals

--- source/tool/valid.h
+++ source/tool/valid.h
@@ -19,18 +19,18 @@ namespace valid
   //    really helpfull tool rubuluar.com
   //
        // Special case to this system
-  static std::regex regDiciplineID("[\\s]*[[:alpha:]0-9_-]+");
+  static std::regex regDiciplineID(R"([\s]*[[:alpha:]0-9_-]+)");
 
       // General cases
-  static std::regex regName   ("[\\s]*[#[:alpha:] ]+");
-  static std::regex regChar3  ("[\\s]*[A-Z]{3}");
-  static std::regex regInt    ("[\\s]*[0-9]+");
-  static std::regex regDouble ("[\\s]*[+-]?[0-9]{0,15}[\\.]?[0-9]{1,15}"); // @robustness - could overflow floating point
-  static std::regex regPhone  ("[\\s]*[+][0-9]{1,3}[ ](([0-9]{8})|(([0-9]{2}[ ]){3}[0-9]{2})|([0-9]{3}[ ][0-9]{2}[ ][0-9]{3}))");
-  static std::regex regTime   ("[\\s]*[0-9]{2}[:][0-9]{2}([:][0-9]{2}([:][0-9]{3})?)?"); // @friendlyness, demands 00:00:00:999 for milliseconds
-  static std::regex regDate   ("[\\s]*(([0][1-9])|([1-2][0-9])|([3][0-1]))[\\.](([0][1-9])|([1][0-2]))[\\.](([0-9]{2})|(([1][9][0-9]{2})|([2][0]([0-9]{2}))))"); //  @robustness - Does not account for varying month-length and year-length
-  static std::regex regMedals ("[\\s]*(([0][1-9]?)|[1-9]{1,2})([-](([0][1-9]?)|[1-9]{1,2})){2}");
-  static std::regex regEmail  ("[\\s]*([A-Za-z0-9\\.-_]{3,128})*[@][A-Za-z0-9\\.]{3,128}[.][a-z]{2,6}");  // @robustness - pseudo email address.
+  static std::regex regName   (R"([\s]*[#[:alpha:] ]+)");
+  static std::regex regChar3  (R"([\s]*[A-Z]{3})");
+  static std::regex regInt    (R"([\s]*[0-9]+)");
+  static std::regex regDouble (R"([\s]*[+-]?[0-9]{0,15}[\.]?[0-9]{1,15})"); // @robustness - could overflow floating point
+  static std::regex regPhone  (R"([\s]*[+][0-9]{1,3}[ ](([0-9]{8})|(([0-9]{2}[ ]){3}[0-9]{2})|([0-9]{3}[ ][0-9]{2}[ ][0-9]{3})))");
+  static std::regex regTime   (R"([\s]*[0-9]{2}[:][0-9]{2}([:][0-9]{2}([:][0-9]{3})?)?)"); // @friendlyness, demands 00:00:00:999 for milliseconds
+  static std::regex regDate   (R"([\s]*(([0][1-9])|([1-2][0-9])|([3][0-1]))[\.]([0][1-9]|[1][0-2])[\.](([1][9])|([2][0]))?([0-9]{2})$)"); //  @robustness - Does not account for varying month-length and year-length
+  static std::regex regMedals (R"([\s]*(([0][1-9]?)|[1-9]{1,2})([-](([0][1-9]?)|[1-9]{1,2})){2})");
+  static std::regex regEmail  (R"([\s]*([A-Za-z0-9\.-_]{3,128})*[@][A-Za-z0-9\.]{3,128}[.][a-z]{2,6})");  // @robustness - pseudo email address.
 
 
   //
